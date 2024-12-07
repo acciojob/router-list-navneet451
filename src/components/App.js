9cc4128767525d10b9302a92c6663557a274fb8e
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-07 19:38:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react'
+import {Routes,Route,Link} from 'react-router-dom'
+import Item1 from './Item1'
+import Item2 from './Item2'
+import Item3 from './Item3'
+import './../styles/App.css'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>List items</h1>
+        <ul>
+            <li><Link to={'/item1'}>Item1</Link></li>
+            <li><Link to={'/item2'}>Item2</Link></li>
+            <li><Link to={'/item3'}>Item3</Link></li>
+        </ul>
+        <Routes>
+            <Route path='/item1' element={<Item1 />}/>
+            <Route path='/item2' element={<Item2 />}/>
+            <Route path='/item3' element={<Item3 />}/>
+        </Routes>
     </div>
   )
 }
